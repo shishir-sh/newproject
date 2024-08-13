7d140181153cc2e5d76376f8b46be7de68502c5d
shishir-sh <57579472+shishir-sh@users.noreply.github.com>
2024-08-14 01:28:46 +0600

    BlogTweet
    # Blog
    
    # HTML
    # CSS
    # BOOTSTRAP
    # DJANGO
    # SQLITE#

---
+++ tweet/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Tweet(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField(max_length=240)
+    photo = models.ImageField(upload_to='photos/', blank=True, null=True)
+    create_at = models.DateField(auto_now_add=True)
+    update_at = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.text[:10]}'
+    
